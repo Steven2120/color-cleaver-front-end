9f79872f2b68fab5fb59a1de4f150ed98ad97d10
Steven Moyano <stevenmoyano1@gmail.com>
2021-02-25 14:07:49 -0600

    Completed front end of app

--- color-cleaver.js
+++ color-cleaver.js
@@ -5,3 +5,22 @@ const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
 
 // Your code here!
+
+const firstColor = getInput(1);
+const secondColor = getInput(2);
+
+if (!isValidPrimary(firstColor, secondColor) || !isValidSecondary(firstColor, secondColor)) {
+    console.log("Those are not colors.");
+}
+if (colorDeconstructor(firstColor)) {
+    console.log(colorDeconstructor(firstColor) + " make this color!");
+}
+if (colorCombinator(firstColor, secondColor)) {
+    console.log(colorCombinator(firstColor, secondColor));
+}
+if (!isValidPrimary(firstColor)) {
+    console.log("That is not a primary color.");
+}
+if (!isValidSecondary(firstColor, secondColor)) {
+    console.log("Those are not secondary colors.");
+}
